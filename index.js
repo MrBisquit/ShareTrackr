10c226a97f405b772bde016c9dbb368e833ddb42
MrBisquit <william@wtdawson.info>
2023-07-26 20:38:48 +0100

    Initial (Basic Analytics, link analytics only)

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * ShareTrackr - V1.0.0
+ * Author - MrBisquit
+ * License - MIT
+ * GitHub - https://github.com/mrbisquit/sharetrackr
+ * NPM - https://npmjs.com/package/sharetrackr
+ */
+
+const ShareTrackr = require("./lib/ShareTrackr");
+
+// Silence :)
+
+module.exports = {
+    ShareTrackr,
+}
